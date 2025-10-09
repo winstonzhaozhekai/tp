74945f288b942d47e49c3f73518b473a10cd91e7
Jared <129586362+Jaredee123@users.noreply.github.com>
2025-10-09 23:51:24 +0800

    Modify delete function to accept contact name

--- src/main/java/seedu/coursebook/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/coursebook/logic/parser/DeleteCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.coursebook.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.coursebook.commons.core.index.Index;
 import seedu.coursebook.logic.commands.DeleteCommand;
 import seedu.coursebook.logic.parser.exceptions.ParseException;
+import seedu.coursebook.model.person.Name;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -17,13 +18,25 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        String trimmed = args == null ? "" : args.trim();
+
+        if (trimmed.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
+
+        // Try parse as index first
+        try {
+            Index index = ParserUtil.parseIndex(trimmed);
+            return new DeleteCommand(index);
+        } catch (ParseException ignored) {
+            // Fall through to parse as name
+        }
+
+        // Parse as name
+        if (!Name.isValidName(trimmed)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+        return new DeleteCommand(new Name(trimmed));
     }
 
 }
